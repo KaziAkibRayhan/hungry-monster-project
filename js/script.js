49ab09abee8b57ce18a642172e488b8c62c2c87b
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2021-02-07 22:41:43 +0600

    Updated Code

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ searchBtn.addEventListener('click', getMealList);
 
 // get meal list that matches with the ingredients
 function getMealList() {
-    let searchInputTxt = document.getElementById('search-input').value;
+    let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
         .then(response => response.json())
         .then(data => {
